2843de8eaeed2c398fd3b103d848eb919611c5f9
Prakharupmannn <upmanjiprakhar@gmail.com>
2025-04-13 01:14:25 +0530

    hello

---
+++ firstClass.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class firstClass {
+    public static void main(String args[]) {
+        // System.out.println("hello world");
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        // int a = 11;
+        // int b = 22;
+        // int sum = a + b;
+        //System.out.println(sum);
+        // int diff = b - a;
+        // System.out.println(diff);
+
+        // int a = 10;
+        // int b = 5;
+        // int ans = (a*b) / (a-b);
+        // System.out.println(ans);
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+        
+        //for taking full name
+        //String name = sc.nextline();
+        //nextInt()
+        //nextFloat()
+
+
+
+    }
+}
