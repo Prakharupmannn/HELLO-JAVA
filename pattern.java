c04bf1f4dcbb514f666394e0b66003c73380cd49
Prakharupmannn <upmanjiprakhar@gmail.com>
2025-04-15 14:04:43 +0530

    downprymid

--- pattern.java
+++ pattern.java
@@ -41,17 +41,32 @@ public static void main(String args[]) {
     //     } System.out.println(" ");
     // }  
 
-    for(int i=1;i<=5;i++){
-        for(int j=5;j>=i;j--){
-            System.out.print(" ");
-        } 
-        for(int k=1;k<=i;k++){
-            System.out.print("*");
+    // for(int i=1;i<=5;i++){
+    //     for(int j=5;j>=i;j--){
+    //         System.out.print(" ");
+    //     } 
+    //     for(int k=1;k<=i;k++){
+    //         System.out.print("*");
+    //     }
+    //     for(int l=1;l<i;l++) {
+    //         System.out.print("*");
+    //     }
+    //     System.out.println(" ");
+    // }
+     
+    for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            } 
+            for(int k=9;k>=i;k--){
+                System.out.print("*");
+            }
+            for(int l=9;l>i;l--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        for(int l=1;l<i;l++) {
-            System.out.print("*");
-        }
-        System.out.println(" ");
-    }
+
+   
 }
 }
