39c504e7d647d8f07c197d2371264a7c3f3cd14a
Prakharupmannn <upmanjiprakhar@gmail.com>
2025-04-21 00:45:39 +0530

    palendrome

--- palendrome.java
+++ palendrome.java
@@ -3,14 +3,14 @@ public class palendrome {
        int n=5;
     
        for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-1;j++){
+        for(int j=1;j<=n-i;j++){
             System.out.print(" ");
         }
         for(int j=i;j>=1;j--){
-            System.out.print(j+ " ");
+            System.out.print(j);
         }
         for(int j=2;j<=i;j++){
-            System.out.print(j + " ");
+            System.out.print(j);
         }
         System.out.println();
        }
