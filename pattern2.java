5577ed2e3dae9cd20a857d8c0426c8c4f34b078c
Prakharupmannn <upmanjiprakhar@gmail.com>
2025-04-21 00:42:03 +0530

    palendrome

--- pattern2.java
+++ pattern2.java
@@ -88,33 +88,68 @@ public class pattern2 {
 
         //BUTTERFLY PATTERN
         //uppper half
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces = 2 * (m-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        } 
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces = 2 * (m-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // } 
         
-        //lower half
-        for(int i=m;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            int spaces = 2 * (m-i);
-            for(int j=1;j<=spaces;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        // //lower half
+        // for(int i=m;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces = 2 * (m-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=m-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //hallow diameter
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=m-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=5;j++){
+        //         if(i==1 || i==5 || j==1 || j==5){
+        //             System.out.print("*");
+        //         } else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m-i;j++){
+            System.out.print(" ");
         }
+        for(int j=1;j<=i;j++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
     }
 }
