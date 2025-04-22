573ee713f3f6c185925f4c2d0d40425fa24722ff
Prakharupmannn <upmanjiprakhar@gmail.com>
2025-04-22 23:38:57 +0530

    Strings

--- string2.java
+++ string2.java
@@ -2,21 +2,39 @@ import java.util.Scanner;
 
 public class string2 {
     public static void main(String[] args) {
-        System.out.println("Enter a name : ");
-        Scanner ch = new Scanner(System.in); 
-        char v = ch.next().charAt(0);
-        switch (v) {
-            case 'a':
-            case 'e':    
-            case 'i':
-            case 'o':
-            case 'u':
-            System.out.println("its a vowel");
-             break;
+        //   Scanner sc = new Scanner(System.in);
+        //   String name = sc.next();
+        //   System.out.println("yoour name is : " + name);
+    
+        //concatenation
+        // String FirstName = "tony";
+        // String lastname = "STARK";
+        // String Fullname = FirstName + "@" + lastname;
+        // String vowel = "a e i o u";
+        // System.out.println(Fullname.length());
+    
+        // //charAt
+        // for(int i=0;i<Fullname.length();i++) {
+        //     System.out.println(Fullname.charAt(i));
+        // }
+        // for(int i=0;i<vowel.length();i++){
+        //     System.out.println(vowel.charAt(i));
+        // }
 
-             default :
-             System.out.println("its a consonent");
-        }
-          
+        //compare
+//         String name1 = "tonty";
+//         String name2 = "tonty";
+
+//         if(name1.compareTo(name2) == 0) {
+//             System.out.println("Strings are equal");
+//         } else {
+//             System.out.println("Strings are not equal");
+//         }
+
+        //substring
+        String sentence = "my name is tony";
+        String name = sentence.substring(3 );
+        System.out.println(name);
     }
 }
+ 
